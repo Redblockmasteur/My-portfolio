0229c56b223e4b9a72a46391a5f5c389bba81e67
Antoine Chatelain <46867831+Redblockmasteur@users.noreply.github.com>
2023-03-03 11:44:05 +0100

    Changed logo

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,14 @@
 export default function Head() {
   return (
     <>
-      <title>Antoine Chatelain Portfolio</title>
+      <title>Antoine Chatelain Portfolio test</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="keywords" content="Antoine Chatelain,Chatelain,Portfolio,Adimaker,Junia,ISEN,"></meta>
       <meta name="description" content="Bienvenue sur le nouveau portfolio d'Antoine Chatelain"></meta>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
     </>
   );
 }
