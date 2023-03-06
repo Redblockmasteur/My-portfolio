81dbcff3401630d003c743c8f6af074ee5e28859
Antoine Chatelain <46867831+Redblockmasteur@users.noreply.github.com>
2023-03-06 22:13:09 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -9,6 +9,7 @@ export default function Head() {
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
       <link rel="manifest" href="/site.webmanifest"/>
+      <meta name="google-site-verification" content="IkHrDyVz30zuB6v__SLYMdhdJrBXjuQ2NHYv1kHQYGo" />
     </>
   );
 }
