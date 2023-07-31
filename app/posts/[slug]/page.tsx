0d59f0f579e29f19b2b02b45f41c12a82c168eee
Antoine Chatelain <46867831+Redblockmasteur@users.noreply.github.com>
2023-07-31 17:01:49 +0200

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -24,7 +24,7 @@ const PostPage = (props: any) => {
     return (
     <div>
         <div className="my-10 text-center">
-            <h2 className=" text-5xl text-slate-600 ">{post.data.title}</h2>
+            <h2 className=" text-3xl md:text-5xl text-slate-600 ">{post.data.title}</h2>
             <p className=" text-lg text-slate-500 mt-2">{post.data.date}</p>
             <p>{post.data.flagEmoji}</p>
         </div>
